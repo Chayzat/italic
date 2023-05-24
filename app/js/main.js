a820a22dccc9233d0629777a4ee34173e06b18c4
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-05-24 16:52:22 +0300

    feat: add basic

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,34 @@ const navList = document.querySelector('.nav-list')
 headerToggle.addEventListener('click', () => {
     navList.classList.toggle('open')
 })
+
+var swiper1 = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  var swiper2 = new Swiper(".trendingSwiper", {
+    slidesPerView: 6,
+    spaceBetween: 10,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper3 = new Swiper(".pressSwiper", {
+    loop: true,
+    slidesPerView: 1,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
